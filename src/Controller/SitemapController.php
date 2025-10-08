2d9522f863d43cd920b6a8069d831169d0b4e177
Sarou <32452874+VincentFerry@users.noreply.github.com>
2025-10-08 15:00:18 +0200

    update SEO

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -4,44 +4,42 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class SitemapController extends AbstractController
 {
     #[Route('/sitemap.xml', name: 'sitemap', methods: ['GET'])]
     public function sitemap(): Response
     {
-        $urls = [
-            [
-                'loc' => $this->generateUrl('app_home', ['_locale' => 'fr'], true),
-                'lastmod' => '2025-01-08',
-                'changefreq' => 'weekly',
-                'priority' => '1.0'
-            ],
-            [
-                'loc' => $this->generateUrl('app_home', ['_locale' => 'en'], true),
-                'lastmod' => '2025-01-08',
-                'changefreq' => 'weekly',
-                'priority' => '1.0'
-            ],
-            [
-                'loc' => $this->generateUrl('app_projects', ['_locale' => 'fr'], true),
-                'lastmod' => '2025-01-08',
-                'changefreq' => 'monthly',
-                'priority' => '0.8'
-            ],
-            [
-                'loc' => $this->generateUrl('app_projects', ['_locale' => 'en'], true),
-                'lastmod' => '2025-01-08',
-                'changefreq' => 'monthly',
-                'priority' => '0.8'
-            ]
-        ];
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://vincentferry.fr/fr</loc>
+        <lastmod>2025-01-08</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://vincentferry.fr/en</loc>
+        <lastmod>2025-01-08</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://vincentferry.fr/fr/projects</loc>
+        <lastmod>2025-01-08</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://vincentferry.fr/en/projects</loc>
+        <lastmod>2025-01-08</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+</urlset>';
 
-        $response = $this->render('sitemap/sitemap.xml.twig', [
-            'urls' => $urls
-        ]);
-
+        $response = new Response($xml);
         $response->headers->set('Content-Type', 'application/xml');
         return $response;
     }
